cf5b5deb82fc0805a8ee213a51131b8eb26bb159
Dhruvil8920 <121276371+Dhruvil8920@users.noreply.github.com>
2023-06-20 23:38:40 +0530

    Update SumOfTwo.py

--- PythonAssignment/SumOfTwo.py
+++ PythonAssignment/SumOfTwo.py
@@ -1,6 +1,5 @@
 """  Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
-
 Input: nums = [1,3,7,21], target = 4
 
 Output: [0,1]
