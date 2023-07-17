640a6ac91c8be05087d964c24feb3f1153827b2c
Dhruvil Gadoya <gadoyadhruvil0@gmail.com>
2023-07-17 15:18:04 +0530

    file changed

---
+++ PythonAssignment/ProductOfArray.py
@@ -0,0 +1,13 @@
+'''
+Given an integer array nums, return an array output such that output[i] is equal to
+the product of all the elements of nums except nums[i].
+
+Example:
+
+Input: [1,2,3,4]
+Output: [24,12,8,6]
+'''
+
+def product_of_array(list):
+    #Write your logic here
+    return list
