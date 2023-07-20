519cbcd4feeab14d3ae1205fb29092ddda4d1924
Dhruvil Gadoya <gadoyadhruvil0@gmail.com>
2023-07-20 21:03:41 +0530

    file changed

--- PythonAssignment/RotateString.py
+++ PythonAssignment/RotateString.py
@@ -4,7 +4,6 @@ Given two strings s and goal,
 return true if and only if s can become goal after some number of shifts on s.
 Input: s = "abcde", goal = "cdeab"
 Output: true
-
 '''
 def rotateString(s, goal):
 
